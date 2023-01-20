294b32c881d2e21f19522a6c5d9d3fa7184cc152
wave2211 <sagarikanikhade073@gmail.com>
2023-01-20 15:45:46 +0530

    JS complete

--- Js-201/masai-ops-617-1333-YVdLEY-b2338fa2e75c2870362fc8dd9bd3c8ee4e329015/scripts/main.js
+++ Js-201/masai-ops-617-1333-YVdLEY-b2338fa2e75c2870362fc8dd9bd3c8ee4e329015/scripts/main.js
@@ -28,7 +28,7 @@ function fetchAndRenderCats(){
         return res.json();
     })
     .then((data)=>{
-        console.log(data);
+        // console.log(data);
 
         let obj= data.map(item =>({
             name :item.name,
@@ -95,3 +95,124 @@ sortZtoABtn.addEventListener("click",()=>{
     // console.log(catsData);
     renderCardList(catsData);
 });
+
+
+// FILTER
+filterLessThan50Btn.addEventListener("click",()=>{
+    let cat=[];
+    for(let i=0;i<catsData.length;i++){
+        if(catsData[i].cost < 50){
+           cat.push(catsData[i]);
+        }
+    }
+    renderCardList(cat);
+});
+
+filterMoreThanEqual50Btn.addEventListener("click",()=>{
+    let cat1=[];
+    for(let i=0;i<catsData.length;i++){
+        if(catsData[i].cost >= 50){
+           cat1.push(catsData[i]);
+        }
+    }
+    renderCardList(cat1);
+});
+
+// RECIPE
+fetchRecipesBtn.addEventListener("click",()=>{
+    fetch(`${baseServerURL}/recipes`)
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+         console.log(data);
+
+         let recipeObj = data.map(item => ({
+            name : item.name,
+            price : "Rs. "+ item.price,
+            image : `${baseServerURL}${item.image}` 
+       }))
+       renderCardListRecipe(recipeObj)
+    })
+});
+
+function renderCardListRecipe(anything) {
+
+    let cardList = `
+      <div class="card-list">
+        ${anything.map(item =>
+            getCardRecipe(item.name, item.price,item.image)
+    ).join(" ")}
+      </div>  
+      `
+
+    mainSection.innerHTML = cardList;
+  }
+
+  function getCardRecipe(name,price,image) {
+    let card = `
+    <div class="card">
+    <div class="card-image">
+      <img src=${image} alt="food"/>
+    </div>
+    <div class="card-body">
+      <h3 class="card-item card-title">${name}</h3>
+    </div>
+    <div class="card-item card-description">
+    ${price}
+    </div>
+  </div>
+  `
+
+    return card;
+  }
+
+
+//    Employee
+fetchEmployeesBtn.addEventListener("click",()=>{
+    fetch(`${baseServerURL}/employees`)
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+         console.log(data);
+
+         let employeeObj = data.map(item => ({
+            name : item.name,
+            salary : "Rs. "+ item.salary,
+            image : `${baseServerURL}${item.image}`,
+            linkText:"Greet" 
+       }))
+       renderCardListEmployee(employeeObj)
+    })
+});
+
+function renderCardListEmployee(anything) {
+
+    let cardList = `
+      <div class="card-list">
+        ${anything.map(item =>
+            getCardEmployee(item.name, item.salary,item.image,item.linkText)
+    ).join(" ")}
+      </div>  
+      `
+
+    mainSection.innerHTML = cardList;
+  }
+
+  function getCardEmployee(name,salary,image,linkText) {
+    let card = `
+    <div class="card">
+    <div class="card-image">
+      <img src=${image} alt="food"/>
+    </div>
+    <div class="card-body">
+      <h3 class="card-item card-title">${name}</h3>
+      <div class="card-item card-description">${salary}</div>
+      <a href="#" class="card-item card-link">${linkText}</a>
+    </div>
+  </div>
+  `
+
+    return card;
+  }
